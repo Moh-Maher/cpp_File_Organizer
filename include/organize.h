2575eb429a05a1fe9596b62d45e7f2c1a2579ccc
Moh-Maher <maherali8932@gmail.com>
2021-11-24 16:00:15 +0100

    comments

--- include/organize.h
+++ include/organize.h
@@ -2,43 +2,36 @@
 #ifndef _ORGANIZE_
 #define _ORGANIZE_
 
-#include<iostream>
 #include<string>
 #include<vector>
-#include <dirent.h>
-#include <filesystem> // C++17
-#include<unistd.h>
 
  
-//namespace fs = std::filesystem;
-using namespace std;
- 
- void funcy(int crrval, int tot, string color, string typ);
+ void funcy(int crrval, int tot, std::string color, std::string typ);
  
 class check{ 
 
 public:
     
 	check(const char*  myfile , int options);
-	void FileExtn(vector<string> File, string  Myfile, bool flag); 
-	void showInfo(int count,string color ,string key);   
-	void Move_File(string f_old, string f_new, string dir_new, bool flag);
+	void FileExtn(std::vector<std::string> File, std::string  Myfile, bool flag); 
+	void showInfo(int count,std::string color ,std::string key);   
+	void Move_File(std::string f_old, std::string f_new, std::string dir_new, bool flag);
     
 private:
 	
-	vector<string> IMAGES = {"jpeg", "jpg", "tiff", "gif", "bmp", "png", "bpg", "svg", "heif", "psd","ebs"};
-	vector<string> VIDEOS = {"avi", "flv", "wmv", "mov", "mp4", "webm", "vob", "mng", "qt", "mpg", "mpeg", "3gp"}; 
-	vector<string> DOCUMENTS  = {"oxps", "epub", "pages", "docx", "doc", "fdf", "ods", "odt", "pwi", "xsn", "xps",
+	std::vector<std::string> IMAGES = {"jpeg", "jpg", "tiff", "gif", "bmp", "png", "bpg", "svg", "heif", "psd","ebs"};
+	std::vector<std::string> VIDEOS = {"avi", "flv", "wmv", "mov", "mp4", "webm", "vob", "mng", "qt", "mpg", "mpeg", "3gp"}; 
+	std::vector<std::string> DOCUMENTS  = {"oxps", "epub", "pages", "docx", "doc", "fdf", "ods", "odt", "pwi", "xsn", "xps",
 	"dotx", "docm", "dox", "rvg", "rtf", "rtfd", "wpd", "xls", "xlsx", "ppt", "ps","epub","js","pdf","pptx"}; 
-	vector<string> ARCHIVES = {"a", "ar", "cpio", "iso", "tar", "gz", "rz", "7z", "dmg", "rar", "xar", "zip","xz"}; 
-	vector<string> AUDIO = {"aac", "aa", "aac", "dvf", "m4a", "m4b", "m4p", "mp3", "rm","msv", "ogg", "oga", "raw", "vox", "wav", "wma"};
-	vector<string> PLAINTEXT = {"txt", "in","dat"};
-	vector<string> SOURCE_CODE = {"f","f90","css","c","h","cc","py","cpp","sh","html5", "html", "htm", "xhtml"};
-	vector<string> OBJECT = {"obj","o"};
-	vector<string> LIBRARY = {"a","so"};
-	vector<string> EXECUTABLE = {"exe","deb","run","out"};
+	std::vector<std::string> ARCHIVES = {"a", "ar", "cpio", "iso", "tar", "gz", "rz", "7z", "dmg", "rar", "xar", "zip","xz"}; 
+	std::vector<std::string> AUDIO = {"aac", "aa", "aac", "dvf", "m4a", "m4b", "m4p", "mp3", "rm","msv", "ogg", "oga", "raw", "vox", "wav", "wma"};
+	std::vector<std::string> PLAINTEXT = {"txt", "in","dat"};
+	std::vector<std::string> SOURCE_CODE = {"f","f90","css","c","h","cc","py","cpp","sh","html5", "html", "htm", "xhtml"};
+	std::vector<std::string> OBJECT = {"obj","o"};
+	std::vector<std::string> LIBRARY = {"a","so"};
+	std::vector<std::string> EXECUTABLE = {"exe","deb","run","out"};
 	  
-	vector<string> files;
+	std::vector<std::string> files;
 	int IMAGE_count=0;
  	int VIDEO_count=0;
         int DOCUMENT_count=0;
--- src/organize.cpp
+++ src/organize.cpp
@@ -1,88 +1,91 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include <dirent.h>
+#include <dirent.h> // to work with directories ( format of directory entries)
 #include <filesystem> // C++17
-#include<unistd.h>
-#include"organize.h"
-#include"layout.h"
+#include"organize.h"  // check class which is the key ingrediant 
+#include"layout.h"  // colors and output metadata
  
-namespace fs = std::filesystem;
+namespace fs = std::filesystem;   // to follow the paths where the created folders should be located
 
-using namespace std;
- 
-void funcy(int crrval, int tot, string color, string typ) {
-string a = BOLD(FRED("#"));
-string b = BOLD(FGRN("###"));
-string c = BOLD(FYEL("###"));
 
- char x ='*';
- int update= (crrval)*100./tot;
+/*----------------------------------------------------------------------------------------------------
+A function counts how many times a specific file has been detected, and print in color the statistics
+
+funcy( how many time a file appear, the total amount of files, output color, the file name)
+-----------------------------------------------------------------------------------------------------*/ 
+void funcy(int crrval, int tot, std::string color, std::string typ)
+{
+   std::string a = BOLD(FRED("#"));
+   std::string b = BOLD(FGRN("###"));
+   std::string c = BOLD(FYEL("###"));
+
+   char x ='*';
+   int update= (crrval)*100./tot;
  
- if (color=="red") {
- cout<<BOLD(FRED("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
- }
+   if (color=="red") {
+	std::cout<<BOLD(FRED("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
  
- if (color=="green") {
- cout<<BOLD(FGRN("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
- }
- if (color=="yellow") {
- cout<<BOLD(FYEL("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
- }
+   if (color=="green") {
+ 	std::cout<<BOLD(FGRN("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
+  
+   if (color=="yellow") {
+ 	std::cout<<BOLD(FYEL("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
  
- if (color=="blue") {
- cout<<BOLD(FBLU("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
+   if (color=="blue") {
+ 	std::cout<<BOLD(FBLU("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
+
+   if (color=="mag") {
+	std::cout<<BOLD(FMAG("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
  
+   if (color=="cyn") { 
+	std::cout<<BOLD(FCYN("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<std::endl;
+   }
 }
-
- if (color=="mag") {
- cout<<BOLD(FMAG("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
  
-}
-
- if (color=="cyn") {
- cout<<BOLD(FCYN("["<<std::string(update/2,x)<<"]"))<<"["<<update<<"%]"<<typ<<": "<<crrval<<" Files"<<endl;
- 
-}
-
-
-}
- 
-check::check(const char*  myfile , int options)
-    {
-        
-        DIR* dir = opendir(myfile);
-        if (dir == NULL)
-        {
-            return;
-        }
+/*----------------------------------------------------------------------------------------------------
+The main constructor of the check class, which makes a call of many functions, it receives the path to
+the targeted folder and read the two different options control the final decision (analyze or organize)  
+-----------------------------------------------------------------------------------------------------*/ 
+check::check(const char*  myfile , int options){
+       
+	DIR* dir = opendir(myfile);  // open a directory 
+	
+	if (dir == NULL){    
+		return;
+   	}
      
-        struct dirent* entity;
-        entity = readdir(dir);
+        struct dirent* entity;   //  This is a structure type used to return information about directory entries.
+        entity = readdir(dir); // read a directory
     
-        while ( entity != nullptr)
+        while ( entity != nullptr)  // while the folder is not empty 
         {
-            files.push_back(entity->d_name);
-            entity = readdir(dir); 
+            files.push_back(entity->d_name); // save the entity name in the vector called files
+            entity = readdir(dir);  // read the next entity
         }
         
-        closedir (dir);
+        closedir (dir); // close the directory
         
-        if (options==1)
+        if (options==1) // Analyze option
         {
-            for (auto file : files)
+            for (auto file : files)  // read all files in the vector files one by one
             {   
-                bool flag = false;
-                FileExtn(IMAGES, file, flag);
-                FileExtn(VIDEOS, file,flag);
-                FileExtn(DOCUMENTS, file,flag); 
-                FileExtn(ARCHIVES, file,flag); 
-                FileExtn(AUDIO, file ,flag);
-                FileExtn(PLAINTEXT, file,flag );
-                FileExtn(SOURCE_CODE, file,flag );
-                FileExtn(OBJECT, file,flag); 
-                FileExtn(LIBRARY, file,flag);
-                FileExtn(EXECUTABLE, file,flag);
+                bool flag = false;                 // to ensure that we just want to analyze not create any folder
+                FileExtn(IMAGES, file, flag);      // check and count how many times the files with extension belong to IMAGES vector appears
+                FileExtn(VIDEOS, file,flag);       // check and count how many times the files with extension belong to VIDEOS vector appears
+                FileExtn(DOCUMENTS, file,flag);    // check and count how many times the files with extension belong to DOCUMENTS vector appears
+                FileExtn(ARCHIVES, file,flag);     // check and count how many times the files with extension belong to ARCHIVES vector appears
+                FileExtn(AUDIO, file ,flag);       // check and count how many times the files with extension belong to AUDIO vector appears
+                FileExtn(PLAINTEXT, file,flag );   // check and count how many times the files with extension belong to PLAINTEXT vector appears
+                FileExtn(SOURCE_CODE, file,flag ); // check and count how many times the files with extension belong to SOURCE_CODE vector appears
+                FileExtn(OBJECT, file,flag);       // check and count how many times the files with extension belong to OBJECT vector appears
+                FileExtn(LIBRARY, file,flag);      // check and count how many times the files with extension belong to LIBRARY vector appears
+                FileExtn(EXECUTABLE, file,flag);   // check and count how many times the files with extension belong to EXECUTABLE vector appears
             }
 
             showInfo(IMAGE_count,"red","IMAGE");
@@ -97,11 +100,11 @@ check::check(const char*  myfile , int options)
             showInfo(EXECUTABLE_count,"mag","EXECUTABLE");
         }
 
-        if (options==2)
+        if (options==2) // Organize option
         {
             for (auto file : files)
             {   
-                bool flag = true;
+                bool flag = true; // to ensure that new folders will be created to move each detected file to it crosspound folder
                 FileExtn(IMAGES, file, flag);
                 FileExtn(VIDEOS, file,flag);
                 FileExtn(DOCUMENTS, file,flag); 
@@ -115,16 +118,16 @@ check::check(const char*  myfile , int options)
             }
         }
        
-        if(options !=1 && options !=2) {cout<<"please set an option!";}
+        if(options !=1 && options !=2) {std::cout<<"please set an option!";}
     }
 
-void check::FileExtn(vector<string> File, string  Myfile, bool flag)
+void check::FileExtn(std::vector<std::string> File, std::string  Myfile, bool flag)
     {    
-        for(int i=0; i<File.size(); i++)
+        for(int i=0; i<File.size(); i++)   //for all the elements of the vector File
         { 
-            if(Myfile.substr(Myfile.find_last_of(".") + 1) == File[i]) 
+            if(Myfile.substr(Myfile.find_last_of(".") + 1) == File[i])  // if an element Myfile has an extension equivelant to the vector element File[i] 
             {
-                if(File==IMAGES) { IMAGE_count++; Move_File(Myfile, Myfile, "IMAGES",flag);}  
+                if(File==IMAGES) { IMAGE_count++; Move_File(Myfile, Myfile, "IMAGES",flag);} //increase the IMAFE_count by one and move the detected file with IMAGE extension to the created folder IMAGES
                 if(File==VIDEOS) {  VIDEO_count++; Move_File(Myfile, Myfile, "VIDEOS",flag);}  
                 if(File==DOCUMENTS) { DOCUMENT_count++; Move_File(Myfile, Myfile, "DOCUMENTS",flag);}  
                 if(File==ARCHIVES) { ARCHIVES_count++; Move_File(Myfile, Myfile, "ARCHIVES",flag);}  
@@ -134,32 +137,27 @@ void check::FileExtn(vector<string> File, string  Myfile, bool flag)
                 if(File==OBJECT) { OBJECT_count++; Move_File(Myfile, Myfile, "OBJECT",flag);}  
                 if(File==LIBRARY) { LIBARARY_count++; Move_File(Myfile, Myfile, "LIBRARY",flag);}  
                 if(File==EXECUTABLE) {  EXECUTABLE_count++; Move_File(Myfile, Myfile, "EXECUTABLE",flag);}         
-            } 
-               
+            }                
         } 
     }
 
-void check::showInfo(int count,string color ,string key)
-    {
-            
+void check::showInfo(int count,std::string color ,std::string key) // print information about the file if its count is greater than its initial valule 0;
+    {            
         if(count>0)
         {   
-        int totcount = IMAGE_count + VIDEO_count + DOCUMENT_count +  ARCHIVES_count + AUDIO_count +
-     PLAINTEXT_count + SOURCE_CODE_count + OBJECT_count + LIBARARY_count +  EXECUTABLE_count ;
-            
-            //cout<<"The number of "<<key<<" files is: "<<count<<endl; 
-            //fs::create_directories(key); 
-            
-            funcy(count, totcount, color, key );
-            
-           
-        }
-        
+           int totcount = IMAGE_count + VIDEO_count + DOCUMENT_count
+                        + ARCHIVES_count + AUDIO_count + PLAINTEXT_count
+                        + SOURCE_CODE_count + OBJECT_count + LIBARARY_count
+                        +  EXECUTABLE_count ;
+           funcy(count, totcount, color, key );   
+        }        
     }
 
-void check::Move_File(string f_old, string f_new, string dir_new, bool flag)
-    {   if(flag==true){
-        fs::create_directory(dir_new);
-        fs::rename(f_old, dir_new+"/"+f_new); 
-        }
+void check::Move_File(std::string f_old, std::string f_new, std::string dir_new, bool flag)
+    {   
+      if(flag==true)
+      {
+      		fs::create_directory(dir_new); //create a new folder 
+       	fs::rename(f_old, dir_new+"/"+f_new);  //move f_old file to the new folder with the new name (chose alway the smae names to avoid files modifications)
+      }
     }
